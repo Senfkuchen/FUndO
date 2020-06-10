01d44c5e9d616ec12a872450c21d016f9eb197af
Olaf Naujocks <ONaujocks@dspace.de>
2020-06-10 12:24:43 +0200

    Change Versuche to Fehlversuche

--- GuessMyWord/GuessMyWord/Program.cs
+++ GuessMyWord/GuessMyWord/Program.cs
@@ -14,25 +14,27 @@ namespace GuessMyWord
             var guessWord = WordGenerator.SelectWord();
             var solvedWord = new string('*', guessWord.Length);
             char key;
-            int count = 0;
+            int countFails = 0;
             do
             { 
                 Console.WriteLine($"Wort : {solvedWord}\n");
-                count++;
                 key = Console.ReadKey().KeyChar;
+                if (!guessWord.Contains(key))
+                {
+                    countFails++;
+                }
+                //Aufbereitung des bisher gelösten Wortes -> solveWord
                 Console.Clear();
                 foreach (var letterTuple in guessWord.Select((x, i) => new { Value = x, Index = i }))
                 {
                     if (letterTuple.Value.Equals(key))
                     {
                         solvedWord = solvedWord.Remove(letterTuple.Index, 1).Insert(letterTuple.Index, key.ToString());
-                    }
+                    }                    
                 }
-
-
             } while (!key.Equals('@') && !solvedWord.Equals(guessWord));
             Console.WriteLine($"Lösungswort: : {guessWord}\n");
-            Console.WriteLine($"Versuche : {count}");
+            Console.WriteLine($"Fehlversuche : {countFails}");
             Console.ReadKey();
         }
     }
